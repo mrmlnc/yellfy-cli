3cb6eeddbdf154283a3f6710119d8abceb4595e5
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-01-17 00:49:20 +0300

    Creating pages and components

--- cli.js
+++ cli.js
@@ -4,66 +4,125 @@
 
 // Node.js libs
 const path = require('path');
+const fs = require('fs-extra');
 // Common libs
 const chalk = require('chalk');
 const meow = require('meow');
+const readPkgUp = require('read-pkg-up');
+const status = require('elegant-status');
 // Local libs
+const files = require('./lib/files');
 const git = require('./lib/git');
 const utils = require('./lib/utils');
 // Common vars
 const cwd = process.cwd();
+const pkg = readPkgUp.sync().pkg;
+// Indicators
+let statusDone = status('Cloning the repository...');
 
 // If the user wants to see a help screen
 const cli = meow({
-  description: `Hello, i'm ${chalk.yellow('Yellfy') }!`,
+  description: `Hello, i'm ${chalk.yellow('Yellfy')}!`,
   help: `
   ${chalk.yellow('Usage:')}
     $ yellfy [<options>]
 
   ${chalk.yellow('Options:')}
-    -h, --help, help  Show help
-    --dir             The deployment instance Yellfy in the specified directory
-    --tag             Install the specified version
-    -i, --install     Start the installation dependencies after you deploy an instance Yellfy
+    -h, --help, help     Show help
+    --dir                The deployment instance Yellfy in the specified directory
+    --tag                Install the specified version
+    -i, --install        Start the installation dependencies after you deploy an instance Yellfy
+    --page               Creating a page (templates + styles)
+    --comp, --component  Creating a component (template + styles)
+    -r                   Overwrite the page or component
 
   ${chalk.yellow('Examples:')}
     $ yellfy --dir=tmp/yellfy
     $ yellfy --tag=1.0.0-a
+    $ yellfy --page=features
 `
 }, {
   alias: {
     h: 'help',
-    i: 'install'
+    i: 'install',
+    comp: 'component'
   }
 });
 
 // Welcome display
-utils.log.yellfy(`Hello, i'm ${chalk.yellow('Yellfy') }!`);
+utils.log.yellfy(`Hello, i'm ${chalk.yellow('Yellfy')}!\n`);
+
+// Creating pages and components
+if (cli.flags.page || cli.flags.comp) {
+  if (pkg === undefined || pkg.name !== 'yellfy') {
+    utils.log.err('Yellfy instance is not found in this directory');
+    process.exit(0);
+  }
+
+  // Page
+  if (cli.flags.page) {
+    const name = cli.flags.page;
+    const pagePath = path.join('app/templates', `${name}.html`);
+    if (!utils.existsSync(pagePath) || cli.flags.r) {
+      files.createPage(name);
+      utils.log.ok(`The page "${name}" was created!`);
+    } else {
+      utils.log.err(`A page with this name ("${name}") already exists. Use "-r" to overwrite it.`);
+    }
+  }
+
+  // Component
+  if (cli.flags.comp) {
+    const name = cli.flags.comp;
+    const compPath = path.join('app/templates/components', `_${name}.html`);
+    if (!utils.existsSync(compPath) || cli.flags.r) {
+      files.createComponent(cli.flags.comp);
+      utils.log.ok(`The component "${name}" was created!`);
+    } else {
+      utils.log.err(`A component with this name ("${name}") already exists. Use "-r" to overwrite it.`);
+    }
+  }
+
+  process.exit(0);
+}
 
 // Clone repo
 const targetDir = (cli.flags.dir) ? path.join(cwd, cli.flags.dir) : cwd;
 git.repoClone('https://github.com/mrmlnc/yellfy', targetDir)
   .then(() => {
-    // If used a specified directory
     if (targetDir !== cwd) {
       process.chdir(targetDir);
     }
 
-    utils.log.success(`Repository cloned into ${chalk.cyan(targetDir)}`);
+    statusDone(true);
+    statusDone = status('Switching version...');
     return git.repoCheckout(cli.flags.tag);
   })
   .then((tag) => {
-    utils.log.success(`The repository switched to the tag ${chalk.blue(tag)}`);
-    return (cli.flags.install) ? utils.installDeps() : 'Setting Yellfy completed!';
+    statusDone(true);
+    if (cli.flags.i) {
+      statusDone = status('Installing dependencies...');
+      return utils.installDeps();
+    }
+
+    statusDone = status('Remove git...');
+    fs.removeSync('.git');
+    statusDone(true);
+    utils.log.ok(`The repository switched to the tag ${chalk.blue(tag)}.`);
+    utils.log.ok(`Setting ${chalk.yellow('Yellfy')} completed!`);
   })
-  .then((info) => {
-    utils.log.success(info);
+  .then(() => {
+    if (cli.flags.i) {
+      statusDone(true);
+      utils.log.ok(`Setting ${chalk.yellow('Yellfy')} completed!`);
+    }
   })
   .catch((err) => {
+    statusDone(false);
     if (err.code === 128) {
       utils.log.err('Destination path is not an empty directory');
       return;
     }
 
-    throw err;
+    console.log(err);
   });
